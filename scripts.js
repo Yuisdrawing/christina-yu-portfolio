fe3a4f7b945904f5eb4ee8a12dc9b738a9cfbbdd
Yuisdrawing <yuisdrawing@gmail.com>
2020-12-17 12:31:42 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// in drawing:
+    // messages pop up on hover on:
+        // coffee mug: Barista
+        // paintbrushes: Painter
+        // posters: Designer
+    // monitor displays a message on hover: Front-end web developer
+
+
+// hamburger menu
+// display X when opened and close on toggle
